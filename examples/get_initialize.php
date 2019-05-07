6c76084198452c614b284870425df551fa2d7128
TERRA GREEN <44187811+TERRAGREEN@users.noreply.github.com>
2019-05-07 16:50:53 +0530

    Add files via upload

--- examples/get_initialize.php
+++ examples/get_initialize.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
 require_once __DIR__ . '/../vendor/config.php'; // Autoload data using config file
-require_once __DIR__ . '/../src/TerraGreen/TGNAPI.php'; // Class Api File
+require_once __DIR__ . '/../src/TerraGreen/TGNAPI.php'; // Api Class File
 
 $api = new TGNAPI($API_KEY, $SECRECT_KEY,$USERNAME,$PASSWORD);
 
