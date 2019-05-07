aec3ba01cada67d6f8b2557fe99edf3f7448e0f3
TERRA GREEN <44187811+TERRAGREEN@users.noreply.github.com>
2019-05-07 16:37:52 +0530

    Update config.php

--- vendor/config.php
+++ vendor/config.php
@@ -1,8 +1,6 @@
 <?php
 //  TerraGreen official API credentials details  //
-
-$API_KEY = "jt8xrwf0F8c02nHEazIzW8jFsYVQUFNWVEdSTlROWEhBU0g0";
-$SECRECT_KEY = "tnJY0vjFylDqHwHTxlgDovvGOUq0aJzfTEZpBaaDmX9QUFNWVEdSTlROWEhBU0g0";
-$USERNAME = "hirenpatel";
-$PASSWORD = "Hiren@123";
-$SEND_ADDRESS  = "f8dcf6c02be0fd70b585c36b915e1f1c";
+$API_KEY = "<API_KEY>";
+$SECRECT_KEY="<SECRECT_KEY>";
+$USERNAME="<WALLET_NAME>";
+$PASSWORD="<PASSWORD>";
