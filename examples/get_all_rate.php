72158e8f044a2e2ee31b6af67945400bd37e8878
TERRA GREEN <44187811+TERRAGREEN@users.noreply.github.com>
2019-06-28 11:36:57 +0530

    Add files via upload

--- examples/get_all_rate.php
+++ examples/get_all_rate.php
@@ -3,7 +3,7 @@ use TerraGreen\TGNAPI;
 require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
 require_once __DIR__ . '/../vendor/config.php'; // Autoload data using config file
 
-$api = new TGNAPI($API_KEY, $SECRECT_KEY,$USERNAME,$PASSWORD);
+$api = new TGNAPI($API_KEY, $SECRECT_KEY,$EMAIL_ADDRESS,$USERNAME,$PASSWORD);
 
 $response =  $api->GetAllRate();
 echo json_encode($response);
