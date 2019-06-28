b31af290d4bc998efbb3cff1a9bace24050bc227
TERRA GREEN <44187811+TERRAGREEN@users.noreply.github.com>
2019-06-28 11:37:43 +0530

    Update config.php

--- vendor/config.php
+++ vendor/config.php
@@ -2,6 +2,7 @@
 //  TerraGreen official API credentials details  //
 $API_KEY = "[API_KEY]";
 $SECRECT_KEY="[SECRECT_KEY]";
+$EMAIL_ADDRESS="[EMAIL_ADDRESS]";
 $USERNAME="[WALLET_NAME]";
 $PASSWORD="[PASSWORD]";
 $SEND_ADDRESS="[SEND_ADDRESS]";
